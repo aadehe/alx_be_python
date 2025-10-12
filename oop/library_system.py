da406ad831da13f496d217238eeaa34298560414
Albert ADEHE <aadehe@gmail.com>
2025-10-12 13:46:36 +0000

    Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,6 +8,9 @@ class Book:
     def details(self):
         return f"Book: {self.title} by {self.author}"
 
+    def __str__(self):
+        return self.details()
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
@@ -16,6 +19,9 @@ class EBook(Book):
     def details(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
+    def __str__(self):
+        return self.details()
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
@@ -23,6 +29,10 @@ class PrintBook(Book):
 
     def details(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+    def __str__(self):
+        return self.details()
+
 
 class Library:
     def __init__(self):
